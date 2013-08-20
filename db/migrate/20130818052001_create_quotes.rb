3ed2b036bcb150e100a2a957bb0f10a84027ef58
James Page <jpage@reckloose.com>
2013-08-20 17:03:12 -0400

    fixed migrations

--- db/migrate/20130818052001_create_quotes.rb
+++ db/migrate/20130818052001_create_quotes.rb
@@ -2,6 +2,7 @@ class CreateQuotes < ActiveRecord::Migration
   def change
     create_table :quotes do |t|
       t.text :name
+      t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20130818052314_create_answers.rb
+++ db/migrate/20130818052314_create_answers.rb
@@ -2,6 +2,8 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :name
+      t.integer :value
+      t.integer :question_id
 
       t.timestamps
     end
