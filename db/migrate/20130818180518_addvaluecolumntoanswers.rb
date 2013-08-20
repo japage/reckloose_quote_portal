d1e6bd73542159dfbdcb629b942bd9df19058ab9
James Page <jpage@reckloose.com>
2013-08-20 02:58:45 -0400

    initial commit

--- db/migrate/20130818180518_addvaluecolumntoanswers.rb
+++
@@ -1,7 +0,0 @@
-class Addvaluecolumntoanswers < ActiveRecord::Migration
-  def change
-  	change_table :answers do |t|
-  		t.integer :value
-	end
-  end
-end
--- db/migrate/20130818182455_removenamefromquotes.rb
+++
@@ -1,5 +0,0 @@
-class Removenamefromquotes < ActiveRecord::Migration
-  def up
-    remove_column :quotes, :name
-  end
-end
